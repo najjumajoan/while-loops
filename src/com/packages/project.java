369a039a729a123430a586ffd5c8203aba2bd34f
Najjuma joan <88086822+najjumajoan@users.noreply.github.com>
2021-09-26 19:58:13 +1200

    while loops

---
+++ src/com/packages/project.java
@@ -0,0 +1,12 @@
+package com.packages;
+
+public class project {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+
+        }
+    }
+}
